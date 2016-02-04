34e6a0f4c6b9788ee86a2489d00126e3e309c091
Egis <e.gecius@gmail.com>
2016-02-04 12:07:48 +0000

    simplifying

--- app/src/main/java/com/egecius/demo_tablayout/MainActivity.java
+++ app/src/main/java/com/egecius/demo_tablayout/MainActivity.java
@@ -1,8 +1,6 @@
 package com.egecius.demo_tablayout;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -11,8 +9,6 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -20,19 +16,9 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
 	/**
-	 * The {@link android.support.v4.view.PagerAdapter} that will provide
-	 * fragments for each of the sections. We use a
-	 * {@link FragmentPagerAdapter} derivative, which will keep every
-	 * loaded fragment in memory. If this becomes too memory intensive, it
-	 * may be best to switch to a
-	 * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-	 */
-	private SectionsPagerAdapter mSectionsPagerAdapter;
-
-	/**
 	 * The {@link ViewPager} that will host the section contents.
 	 */
-	private ViewPager mViewPager;
+	private ViewPager viewPager;
 	private TabLayout tabLayout;
 
 	@Override
@@ -43,41 +29,26 @@ public class MainActivity extends AppCompatActivity {
 		initToolbar();
 		initTabs();
 		initViewPager();
-		initFab();
-	}
-
-	private void initFab() {
-		FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-		fab.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
-			}
-		});
 	}
 
 	private void initViewPager() {
-		mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+		SectionsPagerAdapter mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
-		mViewPager = (ViewPager) findViewById(R.id.container);
-		mViewPager.setAdapter(mSectionsPagerAdapter);
+		viewPager = (ViewPager) findViewById(R.id.container);
+		viewPager.setAdapter(mSectionsPagerAdapter);
 
-		mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+		viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
 		tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
 			@Override
 			public void onTabSelected(TabLayout.Tab tab) {
-				mViewPager.setCurrentItem(tab.getPosition());
+				viewPager.setCurrentItem(tab.getPosition());
 			}
 
 			@Override
-			public void onTabUnselected(TabLayout.Tab tab) {
-
-			}
+			public void onTabUnselected(TabLayout.Tab tab) {}
 
 			@Override
-			public void onTabReselected(TabLayout.Tab tab) {
-
-			}
+			public void onTabReselected(TabLayout.Tab tab) {}
 		});
 	}
 
@@ -94,28 +65,6 @@ public class MainActivity extends AppCompatActivity {
 		tabLayout.addTab(tabLayout.newTab().setText("Tab 2"));
 		tabLayout.addTab(tabLayout.newTab().setText("Tab 3"));
 		tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.menu_main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-
-		//noinspection SimplifiableIfStatement
-		if (id == R.id.action_settings) {
-			return true;
-		}
-
-		return super.onOptionsItemSelected(item);
 	}
 
 	/**
